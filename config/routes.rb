da57ae93a9716943e0f92e6016509391a9f83574
molinojopiento <ofpchile@gmail.com>
2014-09-14 18:42:05 -0300

    Change Gitignore 3

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
-  # Su comentario loco por aca.
+  # Su comentario loco por aca dos.
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
