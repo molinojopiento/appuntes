0ed44ad071f784d0d0db6b15f1d1a12fe9f40b46
molinojopiento <ofpchile@gmail.com>
2014-09-14 18:48:23 -0300

    Cuatro

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
-  # Su comentario loco por aca dos.
+  # Su comentario loco por aca cuatro.
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
